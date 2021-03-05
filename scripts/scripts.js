d26f88c6bc883087fe6bdf0ce6cd3c10591c0476
syahrul12345 <muhdsyahrulnizam123@gmail.com>
2021-03-06 00:04:45 +0800

    Added token lists to watch from

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+const ethTokensList = require('../ETH/tokenlist.json')
+const bscTokensList = require('../BSC/tokenlist.json')
+
+const startScripts = () => {
+  const ethTokens = ethTokensList.tokens
+  const bscTokens = bscTokensList.tokens
+}
+
+startScripts()
